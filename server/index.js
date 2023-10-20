c9fcd9a410a43c9737150398db5f2c77b26230a8
Wessam Mounchit <misterw06@gmail.com>
2023-10-21 01:47:22 +0200

    setup server side

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const { PORT, CLIENT_URL } = require("./constants");
+const passport = require("passport");
+const cors = require("cors");
+
+//INITIALIZE MIDDLEWARES
+app.use(express.json());
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
+app.use(passport.initialize());
+
+// RUN SERVER
+const runApp = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Le serveur tourne au port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Erreur: ${error.message}`);
+  }
+};
+
+runApp();
